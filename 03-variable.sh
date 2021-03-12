594a7ba4d0852c0cab27612d049d5ed22c8cb771
Sam Junaid <ens36256@adobe.com>
2021-03-12 23:21:11 +0000

    03-variable.sh created

---
+++ 03-variable.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Variable : If we assign a name to a set of data is called as Variable
+
+# Declare a variable in bash shell
+NAME=DEVOPS
+
+# Usually other scripting languages will have data types, But shell by default will not have any data types.. All the data is a string for shell.
+
+# Variable names can have character a-z, A-Z, 0-9, _
+
+COUNT=2
+
+# Access the variable  $VAR_NAME / ${VAR_NAME}
+
+echo NAME=$NAME
+echo COUNT=$COUNT
+
+FILE1=sample.txt
+FILE2=mew.txt
+
+#cp $FILE1 $FILE2
