3bef79b935f929179d52b13c2f92e82ea6a4b8e9
Sam Junaid <ens36256@adobe.com>
2021-03-19 20:42:56 +0000

    print

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -10,3 +10,7 @@ if [ "$USER_ID" -ne 0 ]; then
   echo -e "\e[1;31mYou must be a root user to execute this script\e[0m"
   exit 1
 fi
+
+Print {
+  echo "\e[1m $(date +%c) \e[35m$(hostname)\e[0m  \e[1;36m${COMPONENT}" :: $1
+}
