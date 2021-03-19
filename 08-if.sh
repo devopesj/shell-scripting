0b66262549114ae5b249841c8c942635de4b1f75
Sam Junaid <ens36256@adobe.com>
2021-03-19 16:59:47 +0000

    name

--- 08-if.sh
+++ 08-if.sh
@@ -4,8 +4,15 @@ if [ "$USER" != "root" ]; then
   echo Hey, You are not a root user
 fi
 
+read -p 'Enter your Name: ' name
+
+if [ -z "$name"]; then
+  echo Hey, you have not entered your name
+fi
+
 if [ -z "$a"]; then
   echo Variables a is empty
 else
   echo Variable a is not empty. It is "${a}".
+#To unset variable command - "unset (variable)"
 fi
