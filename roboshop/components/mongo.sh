1c361d8c6e44f55a89b577729d089722e4f74e19
Sam Junaid <ens36256@adobe.com>
2021-03-19 16:59:02 +0000

    name

---
+++ roboshop/components/mongo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+
+yum install -y mongodb-org
+systemctl enable mongod
+systemctl start mongod
+
+#Config file from 127 to 0.0.0.0
+
