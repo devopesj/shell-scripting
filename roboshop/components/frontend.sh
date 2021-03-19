c52f28d29aa64784e33272e67c75d8466e2e607a
Sam Junaid <ens36256@adobe.com>
2021-03-19 13:22:31 +0000

    frontend.sh test

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,14 +3,11 @@
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
-
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
 mv static/* .
 rm -rf frontend-master README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
 systemctl restart nginx
