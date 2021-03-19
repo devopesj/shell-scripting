29b3581de88f1b997e8faeff287ffcca70114601
Sam Junaid <ens36256@adobe.com>
2021-03-19 21:13:57 +0000

    maven style

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -25,7 +25,7 @@ Print() {
 }
 
 Stat() {
-   echo -e "[\e[1;34mINFO\e[0m]-----------------------------------------"
+   echo -e "\n[\e[1;34mINFO\e[0m]-----------------------------------------"
   if [ "$1" -eq 0 ]; then
     echo -e "[\e[1;34mINFO\e[0m] \e[1;32mSUCCESS \e[0m"
     else
