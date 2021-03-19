6d33e7e23123a3099770ba9a542b4e4a66bc5a12
Sam Junaid <ens36256@adobe.com>
2021-03-19 20:24:30 +0000

    frontend.shend prints

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+echo -e "\e[34mDownloading Nginx"
 yum install nginx -y
+
+echo -e "\e[34mEnabling and Starting Nginx"
 systemctl enable nginx
 systemctl start nginx
+
+echo -e "\e[34mDownloading fronend zip file"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
 rm -rf *
@@ -11,4 +16,6 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-master README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[34mRestaeting ngninx"
 systemctl restart nginx
