24a3cc70d8bbfe164bcff45b4a10c681b43cd654
Sam Junaid <ens36256@adobe.com>
2021-03-21 18:54:38 +0000

    FILES UPDATED

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -7,7 +7,7 @@ if [ -z "${component}" ]; then
   exit 1
 fi
 
-STATE=${ aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text}
+STATE=$( aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
 
 if [ $STATE != "running" ]; then
   aws ec2 run-instances --launch-template LaunchTemplateId=lt-083666db45a13df5f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,
