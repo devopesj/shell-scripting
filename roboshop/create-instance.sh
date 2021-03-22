ec5ca7b8adef864ad6a7710bd60e5e29814aa4c3
Sam Junaid <ens36256@adobe.com>
2021-03-22 18:36:18 +0000

    Change to create instance

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -12,7 +12,7 @@ STATE=$( aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}
 if [ "$STATE" != "running" ]; then
   aws ec2 run-instances --launch-template LaunchTemplateId=lt-083666db45a13df5f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,
    Value=${component}}]"
-   sleep 20
+   sleep 15
 fi
 
 IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
