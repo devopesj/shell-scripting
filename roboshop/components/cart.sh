3e01e94607a0daca916ad079d432b2690b9355dd
Sam Junaid <ens36256@adobe.com>
2021-03-22 20:59:27 +0000

    cart/redis/user

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
-echo Welcome to Cart
+COMPONENT=cart
+
+source components/common.sh
+
+Print "Installing NodeJS" "yum install nodejs make gcc-c++ -y"
+yum install nodejs make gcc-c++ -y
+Stat $?
+
+Print "Adding RoboShop Project User" "useradd roboshop"
+id roboshop || useradd roboshop
+Stat $?
+
+Print "Download cart Component Code" 'curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"'
+curl -s -L -o /tmp/cart.zip https://github.com/roboshop-devops-project/cart/archive/main.zip
+Stat $?
+
+Print  "Extract cart Component Code" "rm -rf /home/roboshop/cart && cd /home/roboshop && unzip /tmp/cart.zip && mv cart-main cart"
+rm -rf /home/roboshop/cart && cd /home/roboshop && unzip /tmp/cart.zip && mv cart-main cart && cd /home/roboshop/cart
+Stat $?
+
+Print "Install NOdeJS Dependencies" "npm install"
+npm install --unsafe-perm
+Stat $?
+
+chown roboshop:roboshop /home/roboshop -R
+
+Print "Update SystemD Script for cart" "sed -i -e 's/MONGO_DNSNAME/mongodb-ss.devopsb54.tk/' /home/roboshop/cart/systemd.service && mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service"
+sed -i -e 's/MONGO_DNSNAME/mongodb-ss.devopsb54.tk/' /home/roboshop/cart/systemd.service && mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+Stat $?
+
+Print "Start cart Service" "systemctl daemon-reload && systemctl restart cart && systemctl enable cart"
+systemctl daemon-reload && systemctl restart cart && systemctl enable cart
+Stat $?
