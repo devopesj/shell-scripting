2c39cab8bb311f87352e6ff17c444ecfbec5c3a1
Sam Junaid <ens36256@adobe.com>
2021-03-22 18:13:25 +0000

    updated catalogue.sh

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -4,30 +4,33 @@ COMPONENT=catalogue
 
 source components/common.sh
 
-Print "\e[34mInstalling Nodejs\e[0m" "yum install nginx -y"
+Print "Installing NodeJS" "yum install nodejs make gcc-c++ -y"
 yum install nodejs make gcc-c++ -y
 Stat $?
 
-Print "Adding Roboshop project user"
+Print "Adding RoboShop Project User" "useradd roboshop"
 id roboshop || useradd roboshop
 Stat $?
 
-Print "Download Catalogue Component Code" '"curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"'
+Print "Download Catalogue Component Code" 'curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"'
+
 curl -s -L -o /tmp/catalogue.zip https://github.com/roboshop-devops-project/catalogue/archive/main.zip
 Stat $?
 
-Print "Extract Catalogue Component Code" "rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cd /home/roboshop/catalogue && unzip /tmp/catalogue.zip"
-rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cd /home/roboshop/catalogue && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && /home/roboshop/catalogue
+Print  "Extract Catalogue Component Code" "rm -rf /home/roboshop/catalogue && cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue"
+rm -rf /home/roboshop/catalogue && cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && /home/roboshop/catalogue
 Stat $?
 
-Print "Install Nodejs Dependencies" "npm install"
-npm install --unsafe perm
+Print "Install NOdeJS Dependencies" "npm install"
+npm install --unsafe-perm
 Stat $?
 
-Print "Update SystemD Script for Catalogue"
-sed -i -e 's/MONGO_DNSNAME/mongodb-ss.samdevops.tk/' /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+chown roboshop:roboshop /home/roboshop -R
+
+Print "Update SystemD Script for Catalogue" "sed -i -e 's/MONGO_DNSNAME/mongodb-ss.devopsb54.tk/' /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service"
+sed -i -e 's/MONGO_DNSNAME/mongodb-ss.devopsb54.tk/' /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 Stat $?
 
-Print "lets set up the service with systemctl" "Start Catalogue" "systemctl daemon-reload && systemctl start catalogue && systemctl enable catalogue"
-systemctl daemon-reload && systemctl start catalogue && systemctl enable catalogue
+Print "Start Catalogue Service" "systemctl daemon-reload && systemctl restart catalogue && systemctl enable catalogue"
+systemctl daemon-reload && systemctl restart catalogue && systemctl enable catalogue
 Stat $?
