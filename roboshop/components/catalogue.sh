80b9bc29aa6fd243738727e1838a86a5acf12449
Sam Junaid <ens36256@adobe.com>
2021-03-23 12:11:03 +0000

    updated catalogue.sh

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -13,12 +13,11 @@ id roboshop || useradd roboshop
 Stat $?
 
 Print "Download Catalogue Component Code" 'curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"'
-
-curl -s -L -o /tmp/catalogue.zip https://github.com/roboshop-devops-project/catalogue/archive/main.zip
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 Stat $?
 
 Print  "Extract Catalogue Component Code" "rm -rf /home/roboshop/catalogue && cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue"
-rm -rf /home/roboshop/catalogue && cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue
+cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue
 Stat $?
 
 Print "Install NOdeJS Dependencies" "npm install"
