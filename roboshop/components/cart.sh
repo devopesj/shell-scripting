6ad23bfc79c60886a0ae9fab6a30e8318267696a
Sam Junaid <ens36256@adobe.com>
2021-03-23 15:49:04 +0000

    updated cart.sh redis.sh

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -12,12 +12,14 @@ Print "Adding RoboShop Project User" "useradd roboshop"
 id roboshop || useradd roboshop
 Stat $?
 
-Print "Download cart Component Code" 'curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"'
+Print "Download Cart Component Code" 'curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"'
+
 curl -s -L -o /tmp/cart.zip https://github.com/roboshop-devops-project/cart/archive/main.zip
+
 Stat $?
 
-Print  "Extract cart Component Code" "rm -rf /home/roboshop/cart && cd /home/roboshop && unzip /tmp/cart.zip && mv cart-main cart"
-rm -rf /home/roboshop/cart && cd /home/roboshop && unzip /tmp/cart.zip && mv cart-main cart && cd /home/roboshop/cart
+Print  "Extract Cart Component Code" "rm -rf /home/roboshop/cart && cd /home/roboshop && unzip /tmp/cart.zip && mv /home/roboshop/cart-main /home/roboshop/cart && cd /home/roboshop/cart"
+rm -rf /home/roboshop/cart && cd /home/roboshop && unzip /tmp/cart.zip && mv /home/roboshop/cart-main /home/roboshop/cart && cd /home/roboshop/cart
 Stat $?
 
 Print "Install NOdeJS Dependencies" "npm install"
@@ -26,7 +28,7 @@ Stat $?
 
 chown roboshop:roboshop /home/roboshop -R
 
-Print "Update SystemD Script for cart" "sed -i -e 's/REDIS_ENDPOINT/redis-ss.samdevops.tk/' /home/roboshop/cart/systemd.service && mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service"
+Print "Update SystemD Script for cart" "sed -i -e 's/MONGO_DNSNAME/mongodb-ss.samdevops.tk/' /home/roboshop/cart/systemd.service && mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service"
 sed -i -e 's/REDIS_ENDPOINT/redis-ss.samdevops.tk/' -e 's/CATALOGUE_ENDPOINT/catalogue-ss.samdevops.tk/' /home/roboshop/cart/systemd.service && mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
 Stat $?
 
--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -4,7 +4,7 @@ COMPONENT=redis
 
 source components/common.sh
 
-Print "Install Redis Repos" "yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y"
+Print "Install Redis Repos" "yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y"
 yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
 Stat $?
 
@@ -12,10 +12,10 @@ Print "Install Redis" "yum-config-manager --enable remi && yum install redis -y"
 yum-config-manager --enable remi && yum install redis -y
 Stat $?
 
-Print "Update Redis Config" "sed -i -e 's/127.0.0.1/0.0.0.0/ /etc/redis.conf"
+Print "Update Redis Configuration" "sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 Stat $?
 
-Print "Start Redis Database"
+Print "Start Redis Database" "systemctl enable redis && systemctl start redis"
 systemctl enable redis && systemctl start redis
 Stat $?
