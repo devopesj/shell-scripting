289a5439c7dc03818f0d1cbe32b02783922ead87
Sam Junaid <ens36256@adobe.com>
2021-03-23 19:38:00 +0000

    redis.sh /etc/redis/redis.conf file instead of /etc/redis.conf like Raghu

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -14,6 +14,7 @@ Stat $?
 
 Print "Update Redis Configuration" "sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
 Stat $?
 
 Print "Start Redis Database" "systemctl enable redis && systemctl start redis"
