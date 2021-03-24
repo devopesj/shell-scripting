dcc6e4d6346df4c64abe15b760974804ba40847e
devopesj <ope.junaid@gmail.com>
2021-03-24 14:44:46 +0000

    rabbitmq.sh

---
+++ roboshop/components/rabbitmq.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+COMPONENT=rabbitmq
+
+source components/common.sh
+
+Print "Installing Erlang Dependency"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
+Stat $?
+
+Print "Setup YUM Repo"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+Stat $?
+
+Print "Install RabbitMQ" "yum install rabbitmq-server -y "
+yum install rabbitmq-server -y
+Stat $?
+
+Print "Start RabbitMQ" "systemctl enable rabbitmq-server && systemctl start rabbitmq-server"
+systemctl enable rabbitmq-server && systemctl start rabbitmq-server
+Stat $?
+
+rabbitmqctl add_user roboshop roboshop123 && rabbitmqctl set_user_tags roboshop administrator && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+Stat$?
