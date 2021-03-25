f8f18e7305ae844ecbac504de9ad40fb4536526d
devopesj <ope.junaid@gmail.com>
2021-03-25 19:59:37 +0000

    payment.sh

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -17,23 +17,28 @@ cd /home/roboshop
 curl -s -L -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
 Stat $?
 
-Print "Extract payment code"
-rm -rf /home/roboshop/payment && cd /home/roboshop && unzip /tmp/payment.zip && mv payment-main payment && cd payment && unzip /tmp/payment.zip
+Print "Extract payment code" "rm -rf /home/roboshop/payment && cd /home/roboshop && unzip /tmp/payment.zip && mv payment-main payment"
+rm -rf /home/roboshop/payment && cd /home/roboshop && unzip /tmp/payment.zip && mv payment-main payment
 Stat $?
 
-Print "Install the dependencies"
+Print "Installing Python Dependencies"
 cd /home/roboshop/payment && pip3 install -r requirements.txt
 Stat $?
 
-Print "Update SystemD Script for payment" 'sed -i -e "s/CARTENDPOINT/cart-ss.samdevops.tk/" -e "s/DBHOST/mysql-ss.samdevops.tk/" /home/roboshop/payment/systemd.service'
-sed -i -e "s/CARTENDPOINT/cart-ss.samdevops.tk/" -e "s/DBHOST/mysql-ss.samdevops.tk/" /home/roboshop/payment/systemd.service
+Print "Update User details" "USER_ID=$(id -u roboshop)
+GROUP_ID=$(id -g roboshop)
+sed -i -e "/^uid/ c uid=$(USER_ID)" -e "/^gid/ c gid=$(GROUP_ID)" /home/roboshop/payment/payment.ini"
+USER_ID=$(id -u roboshop)
+GROUP_ID=$(id -g roboshop)
+sed -i -e "/^uid/ c uid=$(USER_ID)" -e "/^gid/ c gid=$(GROUP_ID)" /home/roboshop/payment/payment.ini
+
+Print "Update SystemD Script for Payment"
+CARTHOST=$(cart-ss.samdevops.tk)
+USERHOST=$(user-ss.samdevops.tk)
+AMQPHOST=$(rabbitmq-ss.samdevops.tk)
+sed -i -e "s/CARTHOST/$(CARTHOST)" -e "s/USERHOST/$(USERHOST)" -e "s/AMQPHOST/$(AMQPHOST)"
 Stat $?
 
-chown roboshop:roboshop /home/roboshop -R
-
-Print "Copy Service Files And Start The Service"
-mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
-systemctl daemon-reload
-systemctl start payment
-systemctl enable payment
+Print "mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service && systemctl daemon-reload && systemctl enable payment && systemctl start payment"
+mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service && systemctl daemon-reload && systemctl enable payment && systemctl start payment
 Stat $?
